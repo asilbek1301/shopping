e6be26ba9aca71cfb1ac35836e82545fbc3b08b0
asilbek <asilbekotanazarov05@dhu.ac.kr>
2026-04-30 13:50:38 +0900

    feat: thinking about fullstack

---
+++ notice.js
@@ -0,0 +1,13 @@
+/*
+
+FullSatck = Restaurant
+
+Frontend Development:
+    - Backend Server Side Rendering (Backend da Frontend qurish)   <<< DTE / JTE / EJS >>>
+    - Single Page Application                                      <<< React / Angular / Vue >>>
+
+API REQUEST:
+    - Design     -> Traditional API | Rest API | GraphQL API | Webhook | Grpc
+    - Method     -> GET | POST
+    - Structure  -> header | body
+*/
